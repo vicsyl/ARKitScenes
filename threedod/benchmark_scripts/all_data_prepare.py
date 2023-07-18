041817ba87c01c9deaabc7c8214f5a448c550c76
Vaclav Vavra <mr.vaclav.vavra@gmail.com>
2023-07-18 17:18:04 +0200

    all_data_prepare.py

---
+++ threedod/benchmark_scripts/all_data_prepare.py
@@ -0,0 +1,197 @@
+#TODO: remove sys.path based import
+
+#!/usr/bin/env python3
+
+import argparse
+import numpy as np
+import os
+import time
+
+import utils.box_utils as box_utils
+import utils.rotation as rotation
+from utils.taxonomy import class_names, ARKitDatasetConfig
+from utils.tenFpsDataLoader import TenFpsDataLoader, extract_gt
+import utils.visual_utils as visual_utils
+import matplotlib.pyplot as plt
+from PIL import Image
+from pnp_utils import project_from_frame, R_t_from_frame_pose, get_deviation
+
+dc = ARKitDatasetConfig()
+from pathlib import Path
+
+
+def get_scene_ids_gts(data_root):
+    ret = []
+    for p in Path(data_root).iterdir():
+        if p.is_dir() and set(p.name).issubset(set("0123456789")):
+            gt = p.joinpath(f"{p.name}_3dod_annotation.json")
+            if gt.exists() and gt.is_file():
+                ret.append((p.name, gt.absolute().__str__()))
+
+    return ret
+
+
+if __name__ == "__main__":
+    # --data_root ../train/Training
+    # --scene_id 47333462
+    # --gt_path ../train/Training/47333462/47333462_3dod_annotation.json
+    # --output_dir ../train/Training/47333462/47333462_online_prepared_data/ --frame_rate 5
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--data_root",
+        default="./threedod/download/3dod/Training/",
+        help="input folder with ./scene_id"
+        "extracted by unzipping scene_id.tar.gz"
+    )
+
+    parser.add_argument(
+        "--gt_path",
+        default="../../threedod/sample_data/47331606/47331606_3dod_annotation.json",
+        help="gt path to annotation json file",
+    )
+
+    # this shouyld be 1
+    parser.add_argument("--frame_rate", default=1, type=int, help="sampling rate of frames")
+    parser.add_argument(
+        "--output_dir", default="../sample_data/online_prepared_data/", help="directory to save the data and annoation"
+    )
+
+    args = parser.parse_args()
+    scenes = get_scene_ids_gts(args.data_root)
+    print(f"scenes: {scenes}")
+
+    start_time = time.time()
+    for scene_id, gt_path in scenes:
+
+        skipped, boxes_corners, centers, sizes, labels, uids = extract_gt(gt_path)
+        if skipped:
+            print(f"scene {scene_id} skipped")
+            continue
+
+        objects = boxes_corners.shape[0]
+        if objects < 2:
+            print(f"two few objects ({objects}) in scene {scene_id} skipped")
+            continue
+
+        data_path = os.path.join(args.data_root, scene_id, f"{scene_id}_frames")
+        print(f"data_path: {os.path.abspath(data_path)}")
+        loader = TenFpsDataLoader(
+            dataset_cfg=None,
+            class_names=class_names,
+            root_path=data_path,
+            world_coordinate=True
+        )
+
+        start_time_scene = time.time()
+        for i in range(len(loader)):
+
+            if i % args.frame_rate != 0:
+                continue
+
+            frame = loader[i]
+            image_path = frame["image_path"]
+            image = frame["image"]
+            pcd = frame["pcd"]
+            pose = frame["pose"]
+            rgb = frame["color"]
+            #urc, urc_inv = rotation.upright_camera_relative_transform(pose)
+            #urc = pose
+
+            # rotate pcd to urc coordinate
+            # pcd_or = pcd.copy()
+            # pcd = rotation.rotate_pc(pcd, urc)
+
+            # 2. gt_boxes
+            # 2.1 get all boxes to urc coord
+            # 2.2 get box codes
+            boxes = box_utils.corners_to_boxes(boxes_corners)
+            # 2.3 apply a simple box-filter by removing boxes with < 20 points
+            # (n, m)
+            mask_pts_in_box = box_utils.points_in_boxes(pcd, boxes_corners)
+            # (m, )
+            pts_cnt = np.sum(mask_pts_in_box, axis=0)
+            mask_box = pts_cnt > 20
+            gt_labels = {
+                "bboxes": boxes,
+                "bboxes_mask": mask_box,
+                "types": labels,
+                "uids": uids,
+                "pose": pose,
+            }
+
+            # projections = K @ (R @ pcd.T + t_gt)
+            K = frame["intrinsics"]
+            projections2 = project_from_frame(K, pose, pcd).T
+            boxes_crns = project_from_frame(K, pose, boxes_corners.reshape(-1, 3))
+            boxes_crns = boxes_crns.reshape(-1, 8, 3)
+            centers_2d = project_from_frame(K, pose, centers).T
+
+            # pcd_ort = np.vstack(
+            #     (pcd.T, np.ones((1, pcd.T.shape[1])))
+            # )
+            #projections = K @ ((np.linalg.inv(pose) @ pcd_ort)[:3])
+
+            R, t = R_t_from_frame_pose(pose)
+            projections = K @ (R @ pcd.T + t)
+            projections = projections / projections[2]
+            assert np.all(projections2 == projections)
+
+            _, ax = plt.subplots(1, 1, figsize=(9, 16))
+
+            #_, ax = plt.subplots(1, 1)
+            # with open(image_path, "rb") as fid:
+            #     data = Image.open(fid)
+            #     data.load()
+
+            # Show image.
+            img = frame['image']
+            ax.imshow(img)
+            # ax.imshow(data)
+            ax.set_xlim(0, img.shape[0])
+            ax.set_ylim(img.shape[1], 0)
+
+            ax.plot(projections[0],
+                    projections[1],
+                    'r1',
+                    markersize=1)
+
+            # mask_pts_in_box_w = box_utils.points_in_boxes(pcd, boxes_corners)
+            # TODO - arbitrary number of objects
+            color = ["b", "r", "g", "y", "m", "c", "k", "b", "r"]
+            for b_i in range(9):
+                proj_to_use = projections[:, mask_pts_in_box[:, b_i]]
+                fmt = f"{color[b_i]}x"
+                fmt2 = f"{color[b_i]}o"
+                # print(f"fmt: {fmt}")
+
+                ax.plot(proj_to_use[0], proj_to_use[1], fmt, markersize=4)
+
+                centers_display = centers_2d[:, b_i: b_i + 1]
+                centers_display = centers_display[:, centers_display[2] == 1.0]
+                ax.plot(centers_display[0], centers_display[1], fmt2, markersize="20")
+
+                one_box = boxes_crns[b_i].T
+                crns_display = one_box[:, one_box[2] == 1.0]
+                ax.plot(crns_display[0], crns_display[1], fmt, markersize="15")
+
+            np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
+            ax.axis("off")
+            Path(f"imgs/{scene_id}").mkdir(parents=True, exist_ok=True)
+            plt.savefig(f"imgs/{scene_id}/all_{i}.png")
+
+        elapased = time.time() - start_time_scene
+        print(f"{scene_id}: elapsed time: %f sec" % elapased)
+    elapased = time.time() - start_time
+    print(f"total time: %f sec" % elapased)
+
+
+# continue:
+# - all needed data / per scene !!
+# - how to run it on the server?
+# - clean up and externalize
+# - run to generate the data
+# - which frames / scenes it will be used?
+# - measure the time to read the data
+# - adopt to original project (new dataset)
+
+# pose - normalize (assert), deviations..., filters...
--- threedod/benchmark_scripts/pnp_utils.py
+++ threedod/benchmark_scripts/pnp_utils.py
@@ -1,6 +1,21 @@
 import numpy as np
 
 
+def get_deviation(R, unit_vector):
+    assert np.isclose(np.linalg.norm(unit_vector), 1.0)
+    y = R @ unit_vector
+    deviation = np.arccos(y @ unit_vector)
+    return deviation
+
+
+def R_t_from_frame_pose(frame_pose):
+    inv = np.linalg.inv(frame_pose)
+    R = inv[:3, :3]
+    # columns or rows?
+    t = inv[:3, 3:4]
+    return R, t
+
+
 def project_from_frame(K, frame_pose, row_points):
     columns = row_points.T
     augmented = np.vstack(
